7ce562ca6717500e3fbc9f3c7edb9366464916cd
Robin Škába - ZWA server <robin.skaba@gmail.com>
2024-11-27 16:56:27 +0000

    selecting category server

--- php/write.php
+++ php/write.php
@@ -6,6 +6,7 @@ $validator = new Validator();
 $title = $validator->getFromPOST("article-title");
 $summary = $validator->getFromPOST("article-summary");
 $body = $validator->getFromPOST("article-body");
+$category = $validator->getFromPOST("article-category");
 $image = $validator->getFromPOST("article-image");
 
 $validator->checkEmpty("article-title", "Title");
@@ -16,7 +17,7 @@ if ($validator->success()) {
     require_once "database.php";
     $db = new Database();
     $image_path = $db->saveImage($_FILES["article-image"]);
-    $id = $db->addArticle($title, $summary, $body, $image_path, "Politics");
+    $id = $db->addArticle($title, $summary, $body, $image_path, $category);
     
     header("Location: ../php/article.php?id=".$id);
 }
@@ -46,8 +47,6 @@ if ($validator->success()) {
                 <form action="write.php" method="POST" enctype="multipart/form-data">
                     <span id="required-fields-hint">* marked fields are required</span>
 
-                    <!-- TODO pridat vyber kategorie -->
-
                     <label for="article-title">Title *</label>
                     <textarea name="article-title" id="article-title" rows="1" class=<?php $validator->errorClass("article-title") ?>><?= htmlspecialchars($title, ENT_QUOTES) ?></textarea>
                     <label for="article-summary">Summary *</label>
@@ -55,9 +54,20 @@ if ($validator->success()) {
                     <label for="article-body">Content *</label>
                     <textarea name="article-body" id="article-body" rows="10" class=<?php $validator->errorClass("article-body") ?>><?= htmlspecialchars($body, ENT_QUOTES) ?></textarea>
                     
-                    <label for="article-image" id="image-upload">Upload header image *
-                        <input type="file" name="article-image" id="article-image" accept="image/*" class=<?php $validator->errorClass("article-image") ?>>
-                    </label>
+                    <div>
+                        <label for="article-category">Category *</label>
+                        <select name="article-category" id="article-category">
+                            <?php
+                                include "categories.php";
+                                foreach($categories as $_category):
+                            ?>
+                                <option <?php if($_category == $category) echo "selected" ?> value="<?= $_category ?>"><?= $_category ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <label id="image-upload">Upload header image *
+                            <input type="file" name="article-image" id="article-image" accept="image/*" class=<?php $validator->errorClass("article-image") ?>>
+                        </label>
+                    </div>
 
                     <?php $validator->displayErrors() ?>
 
