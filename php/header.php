f100602a33d7c6bfd71aeb1df348e0013e98e0e0
Robin Škába - ZWA server <robin.skaba@gmail.com>
2024-11-28 09:34:01 +0000

    two navs in header

--- php/header.php
+++ php/header.php
@@ -5,27 +5,25 @@ require_once "categories.php";
 ?>
 
 <header>
-    <nav>
-        <div class="top-bar">
-            <div class="cv">
-                <a href="index.html">
-                    <img src="../src/logo_64x64.png" alt="pulsewire logo" id="page-logo">
-                </a>
-                <a class="page-title" href="index.html">PulseWire</a>
-            </div>
-            <div class="account-actions">
-                <a class="account-action" href="administration.php">Admin</a>
-                <a class="account-action" href="write.php">Write</a>
-                <a class="account-action" href="profile.php">Profile</a>
-                <a class="account-action" href="login.php">Login</a>
-                <a class="account-action" href="register.php">Register</a>
-            </div>
-            <img src="../src/menu-icon.png" alt="open category menu" id="category-menu">
+    <nav id="upper-nav">
+        <div>
+            <a href="index.html">
+                <img src="../src/logo_64x64.png" alt="pulsewire logo" id="page-logo">
+            </a>
+            <a class="page-title" href="index.php">PulseWire</a>
         </div>
-        <div class="bottom-bar">
-            <?php foreach($categories as $_category): ?>
-                <a class="category" href=<?= "search_results.php?category=".$_category ?>><?= $_category ?></a>
-            <?php endforeach; ?>
+        <div class="account-actions">
+            <a class="account-action" href="administration.php">Admin</a>
+            <a class="account-action" href="write.php">Write</a>
+            <a class="account-action" href="profile.php">Profile</a>
+            <a class="account-action" href="login.php">Login</a>
+            <a class="account-action" href="register.php">Register</a>
         </div>
+        <img src="../src/menu-icon.png" alt="open category menu" id="category-menu">
+    </nav>
+    <nav id="lower-nav">
+        <?php foreach($categories as $_category): ?>
+            <a class="category" href=<?= "search_results.php?category=".$_category ?>><?= $_category ?></a>
+        <?php endforeach; ?>
     </nav>
 </header>
