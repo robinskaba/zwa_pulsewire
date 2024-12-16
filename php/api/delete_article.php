d2ef632f83d99eea9e01be136d47605a422c2852
Robin Škába - ZWA server <robin.skaba@gmail.com>
2024-12-16 11:26:03 +0000

    log + admin check

--- php/api/delete_article.php
+++ php/api/delete_article.php
@@ -1,9 +1,10 @@
 <?php
 
+require_once "../main/session.php";
 require_once "../main/database.php";
 $db = new Database();
 
-if(isset($_POST["id"])) {
+if(isset($_POST["id"]) && $logged_user && $logged_user->isAdmin()) {
     $article = $db->getArticle($_POST["id"]);
     $db->removeArticle($article);
     header("Location: ../view/index.php");
